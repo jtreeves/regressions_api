c4709cb90f647c0b05f0e477a662c6a3fd00958a
jtreeves <jackson.t.reeves@gmail.com>
2021-01-25 16:57:10 -0500

    [decorators] Readd kwargs

--- app/middleware/decorators.py
+++ app/middleware/decorators.py
@@ -4,14 +4,14 @@ from .searching import searching
 
 def require_apikey(view_function):
     @wraps(view_function)
-    def decorated_function(*args):
+    def decorated_function(*args, **kwargs):
         sent_key = request.args.get('key')
         print(f'SENT_KEY: {sent_key}')
         if sent_key is not None:
             found_key = searching(sent_key)
             print(f'FOUND_KEY: {found_key}')
             if found_key:
-                return view_function(*args)
+                return view_function(*args, **kwargs)
             else:
                 abort(401)
         else:
