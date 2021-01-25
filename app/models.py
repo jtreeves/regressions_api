587c5ef701c2d61fe31aca45ab25b16fcce72650
jtreeves <jackson.t.reeves@gmail.com>
2021-01-25 01:27:19 -0500

    [models] Change table names and add date columns

--- app/models.py
+++ app/models.py
@@ -1,28 +1,29 @@
 from app import db
 from sqlalchemy.dialects.postgresql import JSON, ARRAY
-from sqlalchemy import Table, Column, String, Integer, Float, ForeignKey
+from sqlalchemy import Table, Column, String, Integer, Float, ForeignKey, DateTime
 from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
 class User(db.Model):
-    __tablename__ = 'user'
+    __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True)
     name = Column(String(100))
     email = Column(String(100))
     key = Column(String(100))
+    date = Column(DateTime)
     regressions = relationship('Regression')
 
     def __repr__(self):
         return f'<User {self.id}>'    
 
 class Regression(db.Model):
-    __tablename__ = 'regression'
+    __tablename__ = 'regressions'
 
     id = Column(Integer, primary_key=True)
-    user_id = Column(Integer, ForeignKey('user.id'))
+    user_id = Column(Integer, ForeignKey('users.id'))
     independent = Column(ARRAY(Float))
     dependent = Column(ARRAY(Float))
     linear_coefficients = Column(ARRAY(Float))
@@ -38,6 +39,7 @@ class Regression(db.Model):
     logarithmic_coefficients = Column(ARRAY(Float))
     logarithmic_error = Column(Float)
     best_fit = Column(String)
+    date = Column(DateTime)
 
     def __repr__(self):
         return f'<Regression {self.id}>'
