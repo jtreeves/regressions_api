b3a3adfca60ba9596cac63577e1d4cf597f53bd2
jtreeves <jackson.t.reeves@gmail.com>
2021-01-25 13:26:10 -0500

    [current] Functionalize

--- app/middleware/current.py
+++ app/middleware/current.py
@@ -3,5 +3,7 @@ from app.models import User
 
 current = 'John'
 
-# sent_key = request.args.get('key')
-# current = User.query.filter_by(key=sent_key).first()
+def current(*args):
+    sent_key = request.args.get('key')
+    found_user = User.query.filter_by(key=sent_key).first()
+    return found_user
