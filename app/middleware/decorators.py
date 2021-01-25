60daa377986bf67fcec2da5de02af0db32e606f3
jtreeves <jackson.t.reeves@gmail.com>
2021-01-25 12:46:43 -0500

    [decorators] Abort if input is None

--- app/middleware/decorators.py
+++ app/middleware/decorators.py
@@ -7,10 +7,13 @@ def require_apikey(view_function):
     def decorated_function(*args, **kwargs):
         sent_key = request.args.get('key')
         print(f'SENT_KEY: {sent_key}')
-        found_key = searching(sent_key)
-        print(f'FOUND_KEY: {found_key}')
-        if found_key:
-            return view_function(*args, **kwargs)
+        if sent_key is not None:
+            found_key = searching(sent_key)
+            print(f'FOUND_KEY: {found_key}')
+            if found_key:
+                return view_function(*args, **kwargs)
+            else:
+                abort(401)
         else:
             abort(401)
     return decorated_function
--- app/middleware/searching.py
+++ app/middleware/searching.py
@@ -2,7 +2,7 @@ from app.models import User
 
 def searching(string):
     found_user = User.query.filter_by(key=string).first()
-    if found_user:
+    if found_user is not None:
         return True
     else:
         return False
