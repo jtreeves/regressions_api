f91dc4c11ba8f611e5f30752d3a6ed6c29700ce2
jtreeves <jackson.t.reeves@gmail.com>
2021-01-25 13:47:56 -0500

    [current] Import json to transform returned object

--- app/middleware/current.py
+++ app/middleware/current.py
@@ -1,9 +1,14 @@
 from flask import request
+from json import dumps
 from app.models import User
 
 current = 'John'
 
 def current(*args):
     sent_key = request.args.get('key')
+    print(f'SENT_KEY: {sent_key}')
     found_user = User.query.filter_by(key=sent_key).first()
-    return found_user
+    print(f'FOUND_USER: {found_user}')
+    json_user = dumps(found_user)
+    print(f'JSON_USER: {json_user}')
+    return json_user
