800eca9adc720d16fe866bcf57608e7368089e0c
jtreeves <jackson.t.reeves@gmail.com>
2021-01-25 12:26:41 -0500

    [decorators] Add print statements

--- app/middleware/decorators.py
+++ app/middleware/decorators.py
@@ -7,8 +7,11 @@ def require_apikey(view_function):
     @wraps(view_function)
     def decorated_function(*args, **kwargs):
         sent_key = request.args.get('key')
+        print(f'SENT_KEY: {sent_key}')
         hash_key = hashing(sent_key)
+        print(f'HASH_KEY: {hash_key}')
         found_key = searching(hash_key)
+        print(f'FOUND_KEY: {found_key}')
         if found_key:
             return view_function(*args, **kwargs)
         else:
