3fee09825db38555386aa2061abdf852b7b607a9
jtreeves <jackson.t.reeves@gmail.com>
2021-01-25 01:50:12 -0500

    [forms] Add validation error

--- app/forms.py
+++ app/forms.py
@@ -1,8 +1,14 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import ValidationError, DataRequired, Email
+from app.models import User
 
 class SignUpForm(FlaskForm):
     name = StringField('Name', validators=[DataRequired()])
-    email = StringField('Email', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
     submit = SubmitField('Sign Up')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError('Email address already in use.')
