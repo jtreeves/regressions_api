7beac0d35f4a8807871bdc33fe794c0701a8540a
jtreeves <jackson.t.reeves@gmail.com>
2021-01-25 13:21:09 -0500

    [current] Change to just a single string

--- app/middleware/current.py
+++ app/middleware/current.py
@@ -1,5 +1,7 @@
 from flask import request
 from app.models import User
 
-sent_key = request.args.get('key')
-current = User.query.filter_by(key=sent_key).first()
+current = 'John'
+
+# sent_key = request.args.get('key')
+# current = User.query.filter_by(key=sent_key).first()
