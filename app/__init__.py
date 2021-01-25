2925355bd83ee570d70ade73f7f2fab69636fa85
jtreeves <jackson.t.reeves@gmail.com>
2021-01-25 01:06:41 -0500

    [init] Link to models

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from configurations import Configurations
 import os
 
 app = Flask(__name__)
 app.config.from_object(Configurations)
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
-from app import routes
+from app import routes, models
