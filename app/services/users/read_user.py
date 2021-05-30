9a306cc23b03635b0f3ecb3558a354e47e27b8c4
jtreeves <jackson.t.reeves@gmail.com>
2021-05-30 00:04:21 -0400

    [read_user] Wrap result in conditional, so only return dictionary if user found; otherwise, return False

--- app/services/users/read_user.py
+++ app/services/users/read_user.py
@@ -4,13 +4,17 @@ def read_user(key):
     found_user = User.query.filter_by(
         key = key
     ).first()
-    
-    user_data = {
-        'id': found_user.id,
-        'name': found_user.name,
-        'email': found_user.email,
-        'key': found_user.key,
-        'date': found_user.date
-    }
 
-    return user_data
+    if found_user:
+        user_data = {
+            'id': found_user.id,
+            'name': found_user.name,
+            'email': found_user.email,
+            'key': found_user.key,
+            'date': found_user.date
+        }
+
+        return user_data
+    
+    else:
+        return False
