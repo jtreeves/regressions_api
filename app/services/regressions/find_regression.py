3677afa18c768bc70d89678b0a58c18b8c04317e
jtreeves <jackson.t.reeves@gmail.com>
2021-05-30 13:47:20 -0400

    [find_regression] Add conditional to ensure source included

--- app/services/regressions/find_regression.py
+++ app/services/regressions/find_regression.py
@@ -1,13 +1,17 @@
 from app.models import Regression
 
 def find_regression(user_id, source):
-    found_regression = Regression.query.filter_by(
-        user_id = user_id, 
-        source = source
-    ).first()
+    if source:
+        found_regression = Regression.query.filter_by(
+            user_id = user_id, 
+            source = source
+        ).first()
 
-    if found_regression:
-        return found_regression
+        if found_regression:
+            return found_regression
+        
+        else:
+            return False
     
     else:
-        return False
+        return 'Source must be provided', 403
