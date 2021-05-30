bc3f8ef717293c1426737e36dd6aca65779db597
jtreeves <jackson.t.reeves@gmail.com>
2021-05-30 13:51:32 -0400

    [utilities] Add line breaks for clarity and consistency

--- app/utilities/vet_data_set.py
+++ app/utilities/vet_data_set.py
@@ -6,11 +6,15 @@ def vet_data_set(data_set):
                     for number in point:
                         if isinstance(number, (int, float)):
                             return data_set
+                        
                         else:
                             return 'All numbers within coordinate pairs within data set must be integers or floats', 403
+                
                 else:
                     return 'Each coordinate pair within data set must be a list', 403
+        
         else:
             return 'Data set must be a list', 403
+    
     else:
         return 'Data set must be provided', 403
