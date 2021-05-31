4d9767a04687b21199cb14b32254d8af4913cb6a
jtreeves <jackson.t.reeves@gmail.com>
2021-05-31 19:09:50 -0400

    [generate_regressions] Break up single conditional into two separate conditionals to catch errors from data set and precision separately

--- app/utilities/generate_regression.py
+++ app/utilities/generate_regression.py
@@ -6,64 +6,68 @@ def generate_regression(data_set, precision):
     vetted_data_set = vet_data_set(data_set)
     vetted_precision = vet_precision(precision)
 
-    if not isinstance(vetted_data_set, tuple) and not isinstance(vetted_precision, tuple):
-        results = run_all(vetted_data_set, vetted_precision)
+    if not isinstance(vetted_data_set, tuple):
+        if not isinstance(vetted_precision, tuple):
+            results = run_all(vetted_data_set, vetted_precision)
 
-        linear_coefficients = results['models']['linear']['constants']
-        linear_points = results['models']['linear']['points']
-        linear_correlation = results['models']['linear']['correlation']
-        quadratic_coefficients = results['models']['quadratic']['constants']
-        quadratic_points = results['models']['quadratic']['points']
-        quadratic_correlation = results['models']['quadratic']['correlation']
-        cubic_coefficients = results['models']['cubic']['constants']
-        cubic_points = results['models']['cubic']['points']
-        cubic_correlation = results['models']['cubic']['correlation']
-        hyperbolic_coefficients = results['models']['hyperbolic']['constants']
-        hyperbolic_points = results['models']['hyperbolic']['points']
-        hyperbolic_correlation = results['models']['hyperbolic']['correlation']
-        exponential_coefficients = results['models']['exponential']['constants']
-        exponential_points = results['models']['exponential']['points']
-        exponential_correlation = results['models']['exponential']['correlation']
-        logarithmic_coefficients = results['models']['logarithmic']['constants']
-        logarithmic_points = results['models']['logarithmic']['points']
-        logarithmic_correlation = results['models']['logarithmic']['correlation']
-        logistic_coefficients = results['models']['logistic']['constants']
-        logistic_points = results['models']['logistic']['points']
-        logistic_correlation = results['models']['logistic']['correlation']
-        sinusoidal_coefficients = results['models']['sinusoidal']['constants']
-        sinusoidal_points = results['models']['sinusoidal']['points']
-        sinusoidal_correlation = results['models']['sinusoidal']['correlation']
-        best_fit = results['optimal']['option']
+            linear_coefficients = results['models']['linear']['constants']
+            linear_points = results['models']['linear']['points']
+            linear_correlation = results['models']['linear']['correlation']
+            quadratic_coefficients = results['models']['quadratic']['constants']
+            quadratic_points = results['models']['quadratic']['points']
+            quadratic_correlation = results['models']['quadratic']['correlation']
+            cubic_coefficients = results['models']['cubic']['constants']
+            cubic_points = results['models']['cubic']['points']
+            cubic_correlation = results['models']['cubic']['correlation']
+            hyperbolic_coefficients = results['models']['hyperbolic']['constants']
+            hyperbolic_points = results['models']['hyperbolic']['points']
+            hyperbolic_correlation = results['models']['hyperbolic']['correlation']
+            exponential_coefficients = results['models']['exponential']['constants']
+            exponential_points = results['models']['exponential']['points']
+            exponential_correlation = results['models']['exponential']['correlation']
+            logarithmic_coefficients = results['models']['logarithmic']['constants']
+            logarithmic_points = results['models']['logarithmic']['points']
+            logarithmic_correlation = results['models']['logarithmic']['correlation']
+            logistic_coefficients = results['models']['logistic']['constants']
+            logistic_points = results['models']['logistic']['points']
+            logistic_correlation = results['models']['logistic']['correlation']
+            sinusoidal_coefficients = results['models']['sinusoidal']['constants']
+            sinusoidal_points = results['models']['sinusoidal']['points']
+            sinusoidal_correlation = results['models']['sinusoidal']['correlation']
+            best_fit = results['optimal']['option']
+            
+            new_regression = {
+                'linear_coefficients': linear_coefficients,
+                'linear_points': linear_points,
+                'linear_correlation': linear_correlation,
+                'quadratic_coefficients': quadratic_coefficients,
+                'quadratic_points': quadratic_points,
+                'quadratic_correlation': quadratic_correlation,
+                'cubic_coefficients': cubic_coefficients,
+                'cubic_points': cubic_points,
+                'cubic_correlation': cubic_correlation,
+                'hyperbolic_coefficients': hyperbolic_coefficients,
+                'hyperbolic_points': hyperbolic_points,
+                'hyperbolic_correlation': hyperbolic_correlation,
+                'exponential_coefficients': exponential_coefficients,
+                'exponential_points': exponential_points,
+                'exponential_correlation': exponential_correlation,
+                'logarithmic_coefficients': logarithmic_coefficients,
+                'logarithmic_points': logarithmic_points,
+                'logarithmic_correlation': logarithmic_correlation,
+                'logistic_coefficients': logistic_coefficients,
+                'logistic_points': logistic_points,
+                'logistic_correlation': logistic_correlation,
+                'sinusoidal_coefficients': sinusoidal_coefficients,
+                'sinusoidal_points': sinusoidal_points,
+                'sinusoidal_correlation': sinusoidal_correlation,
+                'best_fit': best_fit
+            }
+            
+            return new_regression
         
-        new_regression = {
-            'linear_coefficients': linear_coefficients,
-            'linear_points': linear_points,
-            'linear_correlation': linear_correlation,
-            'quadratic_coefficients': quadratic_coefficients,
-            'quadratic_points': quadratic_points,
-            'quadratic_correlation': quadratic_correlation,
-            'cubic_coefficients': cubic_coefficients,
-            'cubic_points': cubic_points,
-            'cubic_correlation': cubic_correlation,
-            'hyperbolic_coefficients': hyperbolic_coefficients,
-            'hyperbolic_points': hyperbolic_points,
-            'hyperbolic_correlation': hyperbolic_correlation,
-            'exponential_coefficients': exponential_coefficients,
-            'exponential_points': exponential_points,
-            'exponential_correlation': exponential_correlation,
-            'logarithmic_coefficients': logarithmic_coefficients,
-            'logarithmic_points': logarithmic_points,
-            'logarithmic_correlation': logarithmic_correlation,
-            'logistic_coefficients': logistic_coefficients,
-            'logistic_points': logistic_points,
-            'logistic_correlation': logistic_correlation,
-            'sinusoidal_coefficients': sinusoidal_coefficients,
-            'sinusoidal_points': sinusoidal_points,
-            'sinusoidal_correlation': sinusoidal_correlation,
-            'best_fit': best_fit
-        }
-        
-        return new_regression
+        else:
+            return vetted_precision
 
     else:
-        return 'Either data set or precision incorrect', 403
+        return vetted_data_set
