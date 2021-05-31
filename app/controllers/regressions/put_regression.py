8b4fd0c33f2219a92dcc96346ac2b57eee31c544
jtreeves <jackson.t.reeves@gmail.com>
2021-05-31 19:06:32 -0400

    [put_regression] Add two conditionals to catch errors at both request_submission and update_regression levels

--- app/controllers/regressions/put_regression.py
+++ app/controllers/regressions/put_regression.py
@@ -6,8 +6,18 @@ def put_regression():
     query = request_query()
     submission = request_submission()
 
-    return update_regression(
-        query['user_id'], 
-        query['source'], 
-        submission
-    ), 200
+    if not isinstance(submission, tuple):
+        changed_regression = update_regression(
+            query['user_id'], 
+            query['source'], 
+            submission
+        )
+
+        if not isinstance(changed_regression, tuple):
+            return changed_regression, 200
+        
+        else:
+            return changed_regression
+    
+    else:
+        return submission
