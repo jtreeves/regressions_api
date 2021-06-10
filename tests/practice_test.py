f3087fa619b5ecf6b51b4f780a9ccd9caacd2b08
jtreeves <jackson.t.reeves@gmail.com>
2021-06-10 17:25:34 -0400

    [static/images] Add favicons and other required preview images

--- tests/practice_test.py
+++ tests/practice_test.py
@@ -1,43 +1,23 @@
-# from app import app, db
-# from app.models import User
-# from datetime import datetime
+class TestPages:
+    def test_home(app, client):
+        res = client.get('/')
+        assert res.status_code == 200
 
-# import os
-# import tempfile
-# import pytest
+    def test_about(app, client):
+        res = client.get('/about')
+        assert res.status_code == 200
+    
+    def test_math(app, client):
+        res = client.get('/math')
+        assert res.status_code == 200
+    
+    def test_usage(app, client):
+        res = client.get('/usage')
+        assert res.status_code == 200
 
-# from app import app
-
-def test_index(app, client):
-    del app
-    res = client.get('/')
-    print('RES: ', res)
-    print('RES.DATA: ', res.data)
-    print('RES.DATA[0:10]: ', res.data[0:10])
-    assert res.status_code == 200
-
-# def test_create_user():
-#     new_user = User(
-#         name='Michael',
-#         email='michael@email.com',
-#         key='ABC123',
-#         date=datetime.now()
-#     )
-#     db.session.add(new_user)
-#     db.session.commit()
-
-#     assert new_user.name == 'Michael'
-#     assert new_user.email == 'michael@email.com'
-#     assert new_user.key == 'ABC123'
-
-# def incrementing(num):
-#     return num + 1
-
-# def test_incrementing():
-#     assert incrementing(3) == 5
-
-# def decrementing(num):
-#     return num - 1
-
-# def test_decrementing():
-#     assert decrementing(3) == 1
+class TestPageContents:
+    def test_home_contents(app, client):
+        res = client.get('/')
+        assert b"<header>" in res.data
+        assert b"<footer>" in res.data
+        assert b"so much data in our day-to-day lives" in res.data
