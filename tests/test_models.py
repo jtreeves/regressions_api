89ca6fa417fbab98deea605b0012813e61b64f97
jtreeves <jackson.t.reeves@gmail.com>
2021-06-11 17:47:39 -0400

    [test_models] Add tests for user model

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,5 +1,56 @@
+import pytest
+from datetime import datetime
+from app import db
+from app.models import User, Regression
+
 class TestUserModel:
-    pass
+    def test_creates_user_full(self):
+        new_user = User(
+            name = 'test_user',
+            email = 'test_user@email.com',
+            key = 'ABC123',
+            date = datetime.now()
+        )
+
+        db.session.add(new_user)
+        db.session.commit()
+
+        found_user = User.query.filter_by(
+            email = 'test_user@email.com'
+        ).first()
+
+        assert hasattr(found_user, 'id')
+        assert found_user.key == 'ABC123'
+
+        db.session.delete(found_user)
+        db.session.commit()
+
+    def test_fails_create_user_no_email(self):
+        with pytest.raises(Exception) as exception_info:
+            new_user = User(
+                name='test_fail_user'
+            )
+
+            db.session.add(new_user)
+            db.session.commit() 
+
+        assert 'IntegrityError' in str(exception_info.type)
+        assert 'null value in column "email" of relation "users" violates not-null constraint' in str(exception_info.value)
+
+        db.session.close()
+    def test_fails_create_user_no_name(self):
+        with pytest.raises(Exception) as exception_info:
+            new_user = User(
+                email='test_fail_user@email.com'
+            )
+
+            db.session.add(new_user)
+            db.session.commit() 
+
+        assert 'IntegrityError' in str(exception_info.type)
+        assert 'null value in column "name" of relation "users" violates not-null constraint' in str(exception_info.value)
+
+        db.session.close()
 
 class TestRegressionModel:
     pass
