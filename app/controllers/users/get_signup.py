8487947f0cdcab053cd6e9117bb81cfe5253806a
jtreeves <jackson.t.reeves@gmail.com>
2021-06-11 19:45:28 -0400

    [get_signup] Drill into form object created by SignUpForm class

--- app/controllers/users/get_signup.py
+++ app/controllers/users/get_signup.py
@@ -1,6 +1,12 @@
 from flask import render_template
 
 def get_signup(form):
+    print('FORM IN GET: ', form)
+    print('ALL KEYS IN FORM: ', vars(form))
+    print('FIELDS KEY IN FORM: ', vars(form)['_fields'])
+    print('FIELDS KEY IN FORM: ', vars(form)['_fields'])
+    print('KEY OBJECT IN FORM: ', vars(form)['_fields']['key'])
+    print('KEY VALUE IN FORM: ', vars(form)['_fields']['key'].data)
     return render_template(
         'pages/signup.html', 
         form = form
