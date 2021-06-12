0ad89840fbc04f6ab6542a52d28f45eb4c0e2132
jtreeves <jackson.t.reeves@gmail.com>
2021-06-12 14:45:26 -0400

    [test_forms] Add tests for invalid email types, missing required fields, and other fail cases for signup form

--- tests/test_forms.py
+++ tests/test_forms.py
@@ -90,8 +90,8 @@ class TestSignUpForm:
                 assert submit.data == False
 
     def test_submit_validates_new_signup_form(self, app, client):
-        @app.route("/passing_form", methods=["POST"])
-        def passing_form_route():
+        @app.route("/new_form", methods=["POST"])
+        def new_form_route():
             app.config['WTF_CSRF_ENABLED'] = False
             new_form = signup(
                 name = 'unique',
@@ -104,10 +104,73 @@ class TestSignUpForm:
             }
             return new_form_statuses
 
-        res = client.post("/passing_form")
+        res = client.post("/new_form")
         res_statuses = json.loads(res.data.decode())
         assert res_statuses['submitted']
         assert res_statuses['validated']
+    
+    def test_submit_fails_validate_empty_signup_form(self, app, client):
+        @app.route("/empty_form", methods=["POST"])
+        def empty_form_route():
+            app.config['WTF_CSRF_ENABLED'] = False
+            empty_form = signup()
+            empty_form_statuses = {
+                'submitted': empty_form.is_submitted(),
+                'validated': empty_form.validate_on_submit(),
+                'errors': empty_form.errors
+            }
+            return empty_form_statuses
+
+        res = client.post("/empty_form")
+        res_statuses = json.loads(res.data.decode())
+        assert res_statuses['submitted']
+        assert not res_statuses['validated']
+        assert 'This field is required.' in res_statuses['errors']['name']
+        assert 'This field is required.' in res_statuses['errors']['email']
+        assert 'This field is required.' in res_statuses['errors']['key']
+    
+    def test_submit_fails_validate_partial_signup_form(self, app, client):
+        @app.route("/partial_form", methods=["POST"])
+        def partial_form_route():
+            app.config['WTF_CSRF_ENABLED'] = False
+            partial_form = signup(
+                email = 'partial@email.com'
+            )
+            partial_form_statuses = {
+                'submitted': partial_form.is_submitted(),
+                'validated': partial_form.validate_on_submit(),
+                'errors': partial_form.errors
+            }
+            return partial_form_statuses
+
+        res = client.post("/partial_form")
+        res_statuses = json.loads(res.data.decode())
+        assert res_statuses['submitted']
+        assert not res_statuses['validated']
+        assert 'This field is required.' in res_statuses['errors']['name']
+        assert 'email' not in res_statuses['errors']
+    
+    def test_submit_fails_validate_improper_email_signup_form(self, app, client):
+        @app.route("/improper_email_form", methods=["POST"])
+        def improper_email_form_route():
+            app.config['WTF_CSRF_ENABLED'] = False
+            improper_email_form = signup(
+                name = 'bad email',
+                email = 'improper',
+                key = 'ABC123'
+            )
+            improper_email_form_statuses = {
+                'submitted': improper_email_form.is_submitted(),
+                'validated': improper_email_form.validate_on_submit(),
+                'errors': improper_email_form.errors
+            }
+            return improper_email_form_statuses
+
+        res = client.post("/improper_email_form")
+        res_statuses = json.loads(res.data.decode())
+        assert res_statuses['submitted']
+        assert not res_statuses['validated']
+        assert 'Invalid email address.' in res_statuses['errors']['email']
     
     def test_submit_fails_validate_old_signup_form(self, app, client):
         new_user = User(
@@ -120,8 +183,8 @@ class TestSignUpForm:
         db.session.add(new_user)
         db.session.commit()
 
-        @app.route("/failing_form", methods=["POST"])
-        def failing_form_route():
+        @app.route("/old_form", methods=["POST"])
+        def old_form_route():
             old_form = signup(
                 name = 'test_submit',
                 email = 'an@email.com',
@@ -134,7 +197,7 @@ class TestSignUpForm:
             }
             return old_form_statuses
 
-        res = client.post("/failing_form")
+        res = client.post("/old_form")
         res_statuses = json.loads(res.data.decode())
         assert res_statuses['submitted']
         assert not res_statuses['validated']
