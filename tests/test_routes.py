238bdead3ca3d03237430a0f8b784151bd4f39d6
jtreeves <jackson.t.reeves@gmail.com>
2021-06-12 17:54:30 -0400

    [test_routes] Add test for failing to post if source already in use

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -355,6 +355,83 @@ class TestAPIRoute:
         db.session.delete(found_user)
         db.session.commit()
 
+    def test_api_fails_post_source_unavailable(self, client):
+        new_user = User(
+            name = 'temporary user',
+            email = 'temporary@email.com',
+            key = 'ABC123',
+            date = datetime.now()
+        )
+
+        db.session.add(new_user)
+        db.session.commit()
+
+        found_user = User.query.filter_by(
+            email = 'temporary@email.com'
+        ).first()
+
+        new_regression = Regression(
+            user_id = found_user.id,
+            source = 'TestPostSourceTaken',
+            title = 'Test Post Source Taken Title',
+            independent = 'Test Post Source Taken Independent',
+            dependent = 'Test Post Source Taken Dependent',
+            precision = 4,
+            data_set = [[1, 2], [3, 4], [5, 6]],
+            linear_coefficients = [2, 3],
+            linear_points = {'roots': [[1, 0]], 'inflections': [None]},
+            linear_correlation = 0.5,
+            quadratic_coefficients = [2, 3, 5],
+            quadratic_points = {'roots': [[1, 0], [10, 0]], 'maxima': [[3, 57]]},
+            quadratic_correlation = 0.5,
+            cubic_coefficients = [2, 3, 5, 7],
+            cubic_points = {'roots': [[1, 0], [5, 0], [10, 0]], 'maxima': [[3, 57]]},
+            cubic_correlation = 0.5,
+            hyperbolic_coefficients = [2, 3],
+            hyperbolic_points = {'roots': [[1, 0]], 'maxima': [None]},
+            hyperbolic_correlation = 0.5,
+            exponential_coefficients = [2, 3],
+            exponential_points = {'roots': [None], 'maxima': [None]},
+            exponential_correlation = 0.5,
+            logarithmic_coefficients = [2, 3],
+            logarithmic_points = {'roots': [[1, 0]], 'maxima': [None]},
+            logarithmic_correlation = 0.5,
+            logistic_coefficients = [2, 3, 5],
+            logistic_points = {'roots': [None], 'inflections': [[5, 7]]},
+            logistic_correlation = 0.5,
+            sinusoidal_coefficients = [2, 3, 5, 7],
+            sinusoidal_points = {'roots': [[2, 0], [4, 0]], 'inflections': [[5, 7], [7, 7]]},
+            sinusoidal_correlation = 0.5,
+            best_fit = 'hyperbolic',
+            date = datetime.now()
+        )
+
+        db.session.add(new_regression)
+        db.session.commit()
+
+        found_regression = Regression.query.filter_by(
+            user_id = found_user.id, 
+            source = 'TestPostSourceTaken'
+        ).first()
+
+        res = client.post(
+            '/api?key=ABC123&source=TestPostSourceTaken',
+            json = {
+                'title': 'Test Post Fails when Source Already Taken Title',
+                'independent': 'Test Post Fails when Source Already Taken Independent',
+                'dependent': 'Test Post Fails when Source Already Taken Dependent',
+                'precision': 4,
+                'data_set': [[1, 2], [3, 4], [5, 6], [7, 8], [9, 10], [11, 12], [13, 14], [15, 16], [17, 18], [19, 20]]
+            }
+        )
+
+        assert res.status_code == 409
+        assert b'Source already in use by other collection' in res.data
+
+        db.session.delete(found_regression)
+        db.session.delete(found_user)
+        db.session.commit()
+
     def test_api_returns_put(self, client):
         new_user = User(
             name = 'temporary user',
