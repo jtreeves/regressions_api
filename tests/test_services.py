b0ad63e0dd9fce0d7e9fd4f3e881fe4ade4bcb87
jtreeves <jackson.t.reeves@gmail.com>
2021-06-12 21:58:33 -0400

    [test_services] Import all necessary functions

--- tests/test_services.py
+++ tests/test_services.py
@@ -1,20 +1,28 @@
-class TestCreateUsersService:
+from app.services.users.create_user import create_user
+from app.services.users.read_user import read_user
+from app.services.regressions.create_regression import create_regression
+from app.services.regressions.find_regression import find_regression
+from app.services.regressions.read_regression import read_regression
+from app.services.regressions.update_regression import update_regression
+from app.services.regressions.destroy_regression import destroy_regression
+
+class TestCreateUserService:
     pass
 
-class TestReadUsersService:
+class TestReadUserService:
     pass
 
-class TestCreateRegressionsService:
+class TestCreateRegressionService:
     pass
 
-class TestFindRegressionsService:
+class TestFindRegressionService:
     pass
 
-class TestReadRegressionsService:
+class TestReadRegressionService:
     pass
 
-class TestUpdateRegressionsService:
+class TestUpdateRegressionService:
     pass
 
-class TestDestroyRegressionsService:
+class TestDestroyRegressionService:
     pass
