346ed8efae6b7f3c62d8be1b8e2125e9e22f6b16
jtreeves <jackson.t.reeves@gmail.com>
2021-06-17 12:13:14 -0400

    [test_services] Add test for create_user service, confirming it works if form is filled

--- tests/test_services.py
+++ tests/test_services.py
@@ -1,3 +1,5 @@
+from app import db
+from app.models import User
 from app.services.users.create_user import create_user
 from app.services.users.read_user import read_user
 from app.services.regressions.create_regression import create_regression
@@ -7,7 +9,28 @@ from app.services.regressions.update_regression import update_regression
 from app.services.regressions.destroy_regression import destroy_regression
 
 class TestCreateUserService:
-    pass
+    def test_creates_user_form_filled(self):
+        class Form(object):
+            pass
+        class Field(object):
+            pass
+        
+        new_form = Form()
+        new_form.name = Field()
+        new_form.email = Field()
+        new_form.key = Field()
+        new_form.name.data = 'Test Create User Service Success'
+        new_form.email.data = 'test_create_user_service_success@email.com'
+        new_form.key.data = 'ABC123'
+        
+        user_key = create_user(new_form)
+        assert user_key == 'ABC123'
+
+        found_user = User.query.filter_by(
+            email = 'test_create_user_service_success@email.com'
+        ).first()
+        db.session.delete(found_user)
+        db.session.commit()
 
 class TestReadUserService:
     pass
