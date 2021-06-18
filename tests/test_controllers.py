41dd79d04f3999a00278e7f0f71d9e197356f78a
jtreeves <jackson.t.reeves@gmail.com>
2021-06-18 00:23:16 -0400

    [test_controllers] Add test confirming it fails if source already in use for post_regression controller

--- tests/test_controllers.py
+++ tests/test_controllers.py
@@ -614,6 +614,107 @@ class TestPostRegressionsController:
         db.session.delete(found_regression)
         db.session.delete(found_user)
         db.session.commit()
+    
+    def test_fails_post_regression_old(self, app, client):
+        @app.route("/post_regression_old", methods=["POST"])
+        def post_regression_old_route():
+            post_regression_old = post_regression()
+            return post_regression_old
+        
+        new_user = User(
+            name = 'Test Post Regression Fails Old',
+            email = 'test_post_regression_fails_old@email.com',
+            key = 'ABC123',
+            date = datetime.now()
+        )
+
+        db.session.add(new_user)
+        db.session.commit()
+
+        found_user = User.query.filter_by(
+            email = 'test_post_regression_fails_old@email.com'
+        ).first()
+
+        found_user_id = found_user.id
+
+        new_regression = Regression(
+            user_id = found_user_id,
+            source = 'TestPostRegressionFailsOldSource',
+            title = 'Test Post Regression Fails Old Title',
+            independent = 'Test Post Regression Fails Old Independent',
+            dependent = 'Test Post Regression Fails Old Dependent',
+            precision = 4,
+            data_set = [[1, 2], [3, 4], [5, 6]],
+            linear_coefficients = [2, 3],
+            linear_points = {'roots': [[1, 0]], 'inflections': [None]},
+            linear_correlation = 0.5,
+            quadratic_coefficients = [2, 3, 5],
+            quadratic_points = {'roots': [[1, 0], [10, 0]], 'maxima': [[3, 57]]},
+            quadratic_correlation = 0.5,
+            cubic_coefficients = [2, 3, 5, 7],
+            cubic_points = {'roots': [[1, 0], [5, 0], [10, 0]], 'maxima': [[3, 57]]},
+            cubic_correlation = 0.5,
+            hyperbolic_coefficients = [2, 3],
+            hyperbolic_points = {'roots': [[1, 0]], 'maxima': [None]},
+            hyperbolic_correlation = 0.5,
+            exponential_coefficients = [2, 3],
+            exponential_points = {'roots': [None], 'maxima': [None]},
+            exponential_correlation = 0.5,
+            logarithmic_coefficients = [2, 3],
+            logarithmic_points = {'roots': [[1, 0]], 'maxima': [None]},
+            logarithmic_correlation = 0.5,
+            logistic_coefficients = [2, 3, 5],
+            logistic_points = {'roots': [None], 'inflections': [[5, 7]]},
+            logistic_correlation = 0.5,
+            sinusoidal_coefficients = [2, 3, 5, 7],
+            sinusoidal_points = {'roots': [[2, 0], [4, 0]], 'inflections': [[5, 7], [7, 7]]},
+            sinusoidal_correlation = 0.5,
+            best_fit = 'hyperbolic',
+            date = datetime.now()
+        )
+
+        db.session.add(new_regression)
+        db.session.commit()
+
+        title = 'Test Post Regression Fails Old Title'
+        independent = 'Test Post Regression Fails Old Independent'
+        dependent = 'Test Post Regression Fails Old Dependent'
+        precision = 4
+        data_set = [
+            [1, 2],
+            [3, 4],
+            [5, 6],
+            [7, 8],
+            [9, 10],
+            [11, 12],
+            [13, 14],
+            [15, 16],
+            [17, 18],
+            [19, 20]
+        ]
+
+        res = client.post(
+            "/post_regression_old?key=ABC123&source=TestPostRegressionFailsOldSource",
+            json = {
+                'title': title,
+                'independent': independent,
+                'dependent': dependent,
+                'precision': precision,
+                'data_set': data_set
+            }
+        )
+
+        found_regression = Regression.query.filter_by(
+            user_id = found_user_id, 
+            source = 'TestPostRegressionFailsOldSource'
+        ).first()
+
+        assert res.data == b'Source already in use by other collection'
+        assert res.status_code == 409
+
+        db.session.delete(found_regression)
+        db.session.delete(found_user)
+        db.session.commit()
 
 class TestGetRegressionsController:
     pass
