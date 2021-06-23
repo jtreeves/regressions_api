89a88c5218ed965410ed8832f3ea61982d144f9f
jtreeves <jackson.t.reeves@gmail.com>
2021-06-23 01:06:11 -0400

    [generate_key] Remove '\d' since unnecessary, given '0-9' also included

--- app/utilities/generate_key.py
+++ app/utilities/generate_key.py
@@ -2,5 +2,5 @@ from strgen import StringGenerator
 
 def generate_key():
     """ Use tool to create random string of characters """
-    key = StringGenerator('[a-zA-Z0-9\d]{32}').render()
+    key = StringGenerator('[a-zA-Z0-9]{32}').render()
     return key
