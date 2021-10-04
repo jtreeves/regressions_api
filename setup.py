e4d7d54465d62d282766d3513e071bbd8cba4a2f
jtreeves <jackson.t.reeves@gmail.com>
2021-10-03 23:38:25 -0400

    [requirements] Update regressions library to latest version number (2.1.1)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='regressionz',
-    version='1.0.0',
+    version='1.0.1',
     url='https://github.com/jtreeves/regressions_api',
     license='MIT',
     author='Jackson Reeves',
